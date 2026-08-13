004e21b1a5f9def27087824905160b5cb0522025
sky-ai0304 <kei01201140@gmail.com>
2026-08-14 04:59:04 +0900

    Update post.py

--- post.py
+++ post.py
@@ -238,8 +238,8 @@ def make_chart(candles, filename="xrp_chart.png"):
 
     for i in range(len(closes)):
         part = closes[:i + 1]
-　　　　if len(part) >= 35:
-    　　　 macd_result = calculate_macd(part)
+        if len(part) >= 35: 
+    　　　   macd_result = calculate_macd(part)
 
             if macd_result is not None:
                 m, s, h = macd_result
