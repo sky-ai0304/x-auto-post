1efb929d8ee2d717cb4a5cda51e2b99e7751b1f9
sky-ai0304 <kei01201140@gmail.com>
2026-08-19 21:01:30 +0900

    Update clarity.py

--- clarity.py
+++ clarity.py
@@ -202,7 +202,7 @@ X投稿ルール:
     return response.output_text.strip()
     
     def post_to_x(text):
-    client = tweepy.Client(
+        client = tweepy.Client(
         consumer_key=os.environ["X_API_KEY"],
         consumer_secret=os.environ["X_API_SECRET"],
         access_token=os.environ["X_ACCESS_TOKEN"],
